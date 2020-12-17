adf048d32fb7885f6ea149f30ff953ee72d28a38
rakzool <rahulmob.3607@gmail.com>
2020-12-17 17:37:23 +0530

    home update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../Header/header";
+import "./home.css";
 
 function Home() {
   return (
     <div>
-      <Header />
-      <div className="App">
-        <h1>Hello World</h1>
+      <div className="csi-ka-logo">
+        <Header />
+      </div>
+      <div className="home-tile">
+        <div className="home-title-title">Hola Amigoes</div>
+        <div className="home-title-text">
+          CSI student chapter IPEC is a Society that works towards Nurturing and
+          supporting Talented Coders by providing them Community Support, We
+          also organise Webinars, events, Coding Compititions so as to encourage
+          a learning Environment.
+        </div>
+      </div>
+      <div className="sigs">
+        <div className="temp">Information of sigs go here</div>
       </div>
     </div>
   );
